c7901e64ecd827146ac38a4f5ded83638cc350a1
Karpuram Venu <venukarpuram53@gmail.com>
2022-02-28 01:17:24 +0530

    Initial commit

---
+++ src/stest/ByIdClassNameCssSelector.java
@@ -0,0 +1,36 @@
+package stest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class ByIdClassNameCssSelector {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "E://Program Files//chromedriver_win32/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/form");
+        driver.findElement(By.id("first-name")).sendKeys("Venu");
+        
+                driver.findElement(By.id("last-name")).sendKeys("K");
+        
+                driver.findElement(By.id("job-title")).sendKeys("QA Engineer");
+        
+                driver.findElement(By.id("radio-button-2")).click();
+        
+                driver.findElement(By.id("checkbox-2")).click();
+        
+                driver.findElement(By.cssSelector("option[value='1']")).click();
+        
+                driver.findElement(By.id("datepicker")).sendKeys("05/28/2021");
+                driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);
+        
+                driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
+                Thread.sleep(10000);
+                driver.quit();
+	}
+
+}
---
+++ src/stest/ByXpath.java
@@ -0,0 +1,20 @@
+package stest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class ByXpath {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "E://Program Files//chromedriver_win32/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://moodle.kluniversity.in/login/index.php");
+        driver.findElement(By.name("username")).sendKeys("180040639");
+                driver.findElement(By.name("password")).sendKeys("test123");
+                driver.findElement(By.xpath("//*[@id=\"loginbtn\"]")).click();               
+                Thread.sleep(10000);
+                driver.quit();
+	}
+
+}
